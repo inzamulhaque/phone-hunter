6834bd173b1308e0982c5a5da7d5b989b9090e7a
inzamulhaque <mdihalif@gmail.com>
2022-02-28 23:59:46 +0600

    create basic display phone function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 const searchPhone = () => {
+    document.getElementById("spinnerDiv").style.display = "block";
+    document.getElementById("phoneCards").textContent = "";
     const searchString = document.getElementById("searchInput").value;
-    console.log(searchString);
+    const searchText = searchString.toLowerCase();
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    document.getElementById("searchInput").value = "";
+    fetch(url).then(res => res.json()).then(data => displayPhone(data)).catch(err => console.log("err", err));
+}
+
+// show phone
+const displayPhone = (data) => {
+    document.getElementById("spinnerDiv").style.display = "none";
+    if (data.status === false) {
+        document.getElementById("showResultError").style.display = "block";
+    }
+    document.getElementById("showResultError").style.display = "none";
 }
